b680990561f5c8bfbb71ead174c08362218d889a
TakkAlGhul <takuro.mike@gmail.com>
2019-05-16 22:24:23 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,13 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    respond_to do |format|
+      format.html
+      format.json{ @messages = Message.where('id > ?', params[:id]) }
+    end
   end
+        
+
 
   def create
     @message = @group.messages.new(message_params)
