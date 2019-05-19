a862f4329c56bdaa548a7ede8f986f94ceffb8dc
TakkAlGhul <takuro.mike@gmail.com>
2019-05-19 10:05:32 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,8 +9,8 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
-set :ssh_options, auth_methods: ['takkey'],
-                  keys: ['~/.ssh/takkey.pem']  
+set :ssh_options, auth_methods: ['takey'],
+                  keys: ['~/.ssh/takey.pem']  
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
